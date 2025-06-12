8f9e8853cbc0ba6500acacfe50fa18283bdd4cee
AkshatAga <prags081006@gmail.com>
2025-06-12 13:55:13 +0530

    changed linking to use built in next js package

--- src/components/general/NavBar.jsx
+++ src/components/general/NavBar.jsx
@@ -1,11 +1,11 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
     <nav className="navbar navbar-expand-lg sticky-top bg-dark">
       <div className="container-fluid">
-        <img src="white-logo.png" alt="" className="logo" />
-        <a className="navbar-brand" href="/">
-          OMiNTEL
-        </a>
+        <img src="white-logo.png" alt="omintel-logo" className="logo" />
+        <Link className="navbar-brand" href="/">OMiNTEL</Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,34 +20,34 @@ export default function NavBar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link" href="/#about">
+              <Link className="nav-link" href="/#about">
                 <button
                   type="button"
                   className="btn btn-dark btn-lg px-4 me-md-2"
                 >
                   About
                 </button>
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/#contact-us">
+              <Link className="nav-link" href="/#contact-us">
                 <button
                   type="button"
                   className="btn btn-dark btn-lg px-2 me-md-2"
                 >
                   Contact us
                 </button>
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/partners">
+              <Link className="nav-link" href="/partners">
                 <button
                   type="button"
                   className="btn btn-dark btn-lg px-4 me-md-2"
                 >
                   Partners
                 </button>
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
