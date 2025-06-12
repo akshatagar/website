1eb97f9e44e558fe53ce02e7096dec6a86e1fc45
AkshatAga <prags081006@gmail.com>
2025-06-12 18:04:03 +0530

    added learn more page cards with proper styling, created new json file that can be used to store article data

--- pages/learn-more/index.js
+++ pages/learn-more/index.js
@@ -1,7 +1,24 @@
+import React from 'react';
+import ArticleCard from '../../src/components/learnmorepage/ArticleCard';
+import articlesData from '../../data/articles_data.json';
+
 export default function LearnMore() {
     return (
-        <>
+        <div className="learn-more-container">
             <h1>Learn More</h1>
-        </>
-    )
+            <p>Explore our collection of articles to deepen your knowledge on various topics.</p>
+            
+            <div className="article-cards">
+                {articlesData.map(article => (
+                    <ArticleCard 
+                        key={article.id}
+                        title={article.title}
+                        description={article.description}
+                        imageUrl={article.imageUrl}
+                        articleUrl={article.articleUrl}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
---
+++ src/components/learnmorepage/ArticleCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+const ArticleCard = ({ title, description, imageUrl, articleUrl }) => {
+  return (
+    <div className="article-card">
+      <div className="article-card__image-container">
+        <Image 
+          src={imageUrl} 
+          alt={title}
+          width={400}
+          height={200}
+          className="article-card__image"
+        />
+      </div>
+      <div className="article-card__content">
+        <h3 className="article-card__title">{title}</h3>
+        <p className="article-card__description">{description}</p>
+        <Link href={articleUrl} className="article-card__link">
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleCard; 
