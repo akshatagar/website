bcc41dda9412bf68085d122f841c4978849c9b8b
AkshatAga <prags081006@gmail.com>
2025-06-14 17:58:39 +0530

    testing to see if navbar image will load faster

--- src/components/general/NavBar.jsx
+++ src/components/general/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
     <nav className="navbar navbar-expand-lg sticky-top bg-dark">
       <div className="container-fluid">
         <Link href="/">
-          <Image width={40} height={40} src="/white-logo.png" alt="omintel-logo" className="logo" />
+          <Image width={40} height={40} src="/white-logo.png" alt="omintel-logo" className="logo" priority />
         </Link>
         <Link className="navbar-brand" href="/">
           OMiNTEL
