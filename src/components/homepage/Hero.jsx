3d3efd7f41b089d4a16bcd00c16ea7b0424103d2
AkshatAga <prags081006@gmail.com>
2025-06-14 17:22:51 +0530

    moved text to left side of graphic

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -14,17 +14,15 @@ export default function Hero() {
 
   return (
     <div className="container col-xxl-8 py-5 hero">
-      <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
-        <div className="col-10 col-sm-8 col-lg-6"></div>
-
-        {/*rest of the text*/}
-        <div className="col-lg-8">
+      <div className="row align-items-center g-5 py-5">
+        {/* Text Column - Left Side */}
+        <div className="col-lg-6">
           <h1 className="display-5 fw-bold lh-1 mb-3 hero-header">
             AI Based Solutions
           </h1>
         </div>
 
-        {/* Diagram Column - Right Side (Contained) */}
+        {/* Diagram Column - Right Side */}
         <div className="col-lg-6">
           <div className="diagram-container">
             <div className="interactive-diagram">
