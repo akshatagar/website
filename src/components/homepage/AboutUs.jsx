47a9769917d8aa4e99bb30d3ee526fa47f149cd2
VedBhatnagar03 <vedrb2@illinois.edu>
2025-07-12 12:47:08 +0400

    fixed layout for diagram and how we do it

--- src/components/homepage/AboutUs.jsx
+++ src/components/homepage/AboutUs.jsx
@@ -32,83 +32,81 @@ export default function AboutUs({aboutUs, partners}) {
 
     return (
       <section id="about" className="about-section py-5">
-        <div className="row">
-        <div className={styles.diagramContainer + " col-md-6"}>
-            <div className={styles.interactiveDiagram}>
-
-            
-              <div className={`${styles.centralLogo} ${isClicked ? styles.clicked : ''}`}
-                onClick={handleCentralClick}
-                style={{ cursor: 'pointer' }}
-              >
-                <Image
-                  src="/Logo_with_white.png" 
-                  alt="AI Core" 
-                  className="img-fluid"
-                  width={120}
-                  height={120}
-                  priority={true}
-                />
-              </div>
+        <div className='container'>
+          <div className="row align-items-center flex-nowrap gx-5">
 
 
-              {/* Satellite Logos */}
-              {nodes.map((node) => (
-                <div 
-                  key={node.id}
-                  className={`${styles.satelliteLogo} ${isClicked ? styles.scattered : ''}`}
-                  style={{
-                    top: node.top,
-                    left: node.left,
-                    width: `calc(${node.size} + 30px)`,
-                    height: `calc(${node.size} + 30px)`,
-                    '--final-top': node.top,
-                    '--final-left': node.left,
-                    '--size': node.size,
-                    '--delay': `${node.id * 0.1}s`                    
-                  }}
+            <div className={styles.diagramContainer + " col-lg-6 mb-4 mb-lg-0"}>
+              <div className={styles.interactiveDiagram}>
+
+                <div className={`${styles.centralLogo} ${isClicked ? styles.clicked : ''}`}
+                  onClick={handleCentralClick}
+                  style={{ cursor: 'pointer' }}
                 >
-                  <Link href={{
-                    pathname:'/partners',
-                    query: { id: node.id}
-                  }}>
-                  <img 
-                    src={node.img} 
-                    alt={node.name} 
-                    className={styles.satelliteImg}
-                    style={{ 
-                      width: node.size,
-                      height: node.size
-                    }}
+                  <Image
+                    src="/Logo_with_white.png" 
+                    alt="AI Core" 
+                    className="img-fluid"
+                    width={120}
+                    height={120}
+                    priority={true}
                   />
-                  </Link>
-                  <span className={styles.satelliteLabel}>{node.name}</span>
                 </div>
-              ))}
+
+                {nodes.map((node) => (
+                  <div 
+                    key={node.id}
+                    className={`${styles.satelliteLogo} ${isClicked ? styles.scattered : ''}`}
+                    style={{
+                      top: node.top,
+                      left: node.left,
+                      width: `calc(${node.size} + 30px)`,
+                      height: `calc(${node.size} + 30px)`,
+                      '--final-top': node.top,
+                      '--final-left': node.left,
+                      '--size': node.size,
+                      '--delay': `${node.id * 0.1}s`                    
+                    }}
+                  >
+                    <Link href={{
+                      pathname:'/partners',
+                      query: { id: node.id}
+                    }}>
+                    <img 
+                      src={node.img} 
+                      alt={node.name} 
+                      className={styles.satelliteImg}
+                      style={{ 
+                        width: node.size,
+                        height: node.size
+                      }}
+                    />
+                    </Link>
+                    <span className={styles.satelliteLabel}>{node.name}</span>
+                  </div>
+                ))}
               </div>
             </div>
 
-    {/* How we do it Section */}
-<div className="container">
-  <div className="row align-items-center">
-    <div className="col-md-6">
-            <h3 className="text-white"
-            style={{
+
+            <div className="col-md-7">
+              <h3 className="text-white"
+              style={{
               paddingBottom: '3rem',
-            }}>
+              }}>
               How we do it?
-            </h3>
-      <ul className={styles.processList}>
-        {aboutUs["howWeDoIt"].map((item, index) => (
-          <li key={index}>{item}
-          </li>
-        ))}
-      </ul>
-      </div>
-    </div>
-  </div>
-</div>
-</section>
+              </h3>
+              <ul className={styles.processList}>
+                {aboutUs["howWeDoIt"].map((item, index) => (
+                <li key={index} className='mb-3'>{item}
+                </li>
+                ))}
+              </ul>
+            </div>
+            
+          </div>
+        </div>
+      </section>
         
   )
 }
