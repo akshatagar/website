afff103d87fa8f81f8365d7cacce58786ef516a0
VedBhatnagar03 <vedrb2@illinois.edu>
2025-07-24 13:57:06 +0400

    feedback opens on all pages and is now transparent

--- src/layout/main-layout.jsx
+++ src/layout/main-layout.jsx
@@ -1,13 +1,25 @@
 import NavBar from "../components/general/NavBar";
 import Footer from "../components/general/Footer";
+import FeedbackForm from "../components/feedbackpage/FeedbackForm";
+import { useFeedback } from '../components/contexts/FeedbackContext';
+import PopupWindow from "../components/feedbackpage/Popup";
 
 export default function MainLayout({ children }) {
+  
+  const { showFeedback, setShowFeedback } = useFeedback();
   return (
     <div className="d-flex flex-column min-vh-100 w-100 overflow-hidden">
       <NavBar />
       <main className="flex-grow-1 w-100">
         {children}
       </main>
+      {showFeedback && (
+        <PopupWindow
+          title="Feedback Form"
+          onClose={() => setShowFeedback(false)}>
+          <FeedbackForm />
+         </PopupWindow>
+      )}
       <Footer />
     </div>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,15 @@ import Hero from "../components/homepage/Hero"
 import AboutUs from "../components/aboutpage/AboutUs"
 import { client } from '@/lib/sanity'
 import { useRouter } from 'next/router';
-import FeedbackForm from "../components/feedbackpage/FeedbackForm";
-import { useFeedback } from '../components/contexts/FeedbackContext';
-import PopupWindow from "../components/feedbackpage/Popup";
+
 
 
 export default function Home( { partners }) {
 
-  const { showFeedback, setShowFeedback } = useFeedback();
 
     return (
         <>
             <Hero partners={partners}/>
-            {showFeedback && (
-            <PopupWindow
-              title="Feedback Form"
-              onClose={() => setShowFeedback(false)}>
-              <FeedbackForm />
-            </PopupWindow>
-          )}
         </>
     )
 }
